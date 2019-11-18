94d2eb73203c343d9f9b8025b15a83c2ce9b214e
TheSharkReadabook <efride710@gmail.com>
2019-11-19 01:28:16 +0900

    index

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const ejs = require('ejs')
 
+
+const index = require('./routes/index')
+
+const PORT = 3000
 
 app.use('/', index)
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, () => {
+    console.log(PORT)
+})
